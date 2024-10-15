332fd6b98799d16ab46f28d2c626c556c38278d8
Antti Juustila <antti.juustila@gmail.com>
2024-10-15 07:59:54 +0300

    Initial Commit

---
+++ CourseDeadlineCounter/CourseDeadlineCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseDeadlineCounterApp.swift
+//  CourseDeadlineCounter
+//
+//  Created by Antti Juustila on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CourseDeadlineCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CourseDeadlineCounterTests/CourseDeadlineCounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CourseDeadlineCounterTests.swift
+//  CourseDeadlineCounterTests
+//
+//  Created by Antti Juustila on 15.10.2024.
+//
+
+import Testing
+@testable import CourseDeadlineCounter
+
+struct CourseDeadlineCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
